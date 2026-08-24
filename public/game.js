86d6427c2cf572480669faf5a42f509d0832304d
Rule-Book <trunkz77@live.com>
2026-08-24 00:39:17 -0400

    setup localstorage cookie load/creation

--- public/game.js
+++ public/game.js
@@ -1,1 +1,17 @@
 console.log('game.js loaded');
+
+function getUserId() {
+	const key = 'game_user_id';
+
+	let id = localStorage.getItem(key);
+
+	if (!id) {
+		id = crypto.randomUUID();
+		localStorage.setItem(key, id);
+		console.log(`Generated new game_user_id: ${id}`);
+	} else {
+		console.log(`Found existing game_user_id: ${id}`);
+	}
+}
+
+getUserId();
