5efbb09eb5e235749b1cae102e517e7477495610
Rule-Book <trunkz77@live.com>
2026-09-22 13:53:48 -0400

    migrate hosting of html+js to CloudFront Distribution

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 //serve everything in the public folder
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.get('/', (req, res) => {
